d1ad81a78b6efbe28fae697dd61e6b7786a6d46a
Wang <131781679+WnagZ@users.noreply.github.com>
2023-10-03 20:01:52 +0200

    Update wifi.ino

--- arduino/wifi/wifi.ino
+++ arduino/wifi/wifi.ino
@@ -1,9 +1,12 @@
 void setup() {
-  // put your setup code here, to run once:
-
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
 }
-
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  char incomingByte;
+   // If there is a data stored in the serial receive buffer, read it and print it to the serial port as human-readable ASCII text.
+  if(Serial.available()){  
+    incomingByte = Serial.read();
+    Serial.print(incomingByte);  
+  }
 }
