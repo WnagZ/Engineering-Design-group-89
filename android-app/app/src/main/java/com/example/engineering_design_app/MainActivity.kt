b7a4b26ff5b9958ea02407510908a6fde6f601be
Wang <131781679+WnagZ@users.noreply.github.com>
2023-10-03 20:01:17 +0200

    First setup connection Arduino

---
+++ android-app/app/src/main/java/com/example/engineering_design_app/MainActivity.kt
@@ -0,0 +1,72 @@
+package com.example.engineering_design_app
+
+import android.hardware.usb.UsbDevice
+import android.os.Bundle
+import android.os.Handler
+import android.text.method.ScrollingMovementMethod
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import me.aflak.arduino.Arduino
+import me.aflak.arduino.ArduinoListener
+
+
+class MainActivity : AppCompatActivity(), ArduinoListener {
+    private var arduino: Arduino? = null
+    private var displayTextView: TextView? = null
+    private var editText: EditText? = null
+    private var sendBtn: Button? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        displayTextView = findViewById(R.id.diplayTextView)
+        editText = findViewById(R.id.editText)
+        sendBtn = findViewById(R.id.sendBtn)
+        displayTextView?.movementMethod = ScrollingMovementMethod()
+        sendBtn?.setOnClickListener {
+            val editTextString = editText?.text.toString()
+            arduino!!.send(editTextString.toByteArray())
+            editText?.text?.clear()
+        }
+        arduino = Arduino(this)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        arduino!!.setArduinoListener(this)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        arduino!!.unsetArduinoListener()
+        arduino!!.close()
+    }
+
+    override fun onArduinoAttached(device: UsbDevice) {
+        display("arduino attached...")
+        arduino!!.open(device)
+    }
+
+    override fun onArduinoDetached() {
+        display("arduino detached.")
+    }
+
+    override fun onArduinoMessage(bytes: ByteArray) {
+        display(kotlin.String())
+    }
+
+    override fun onArduinoOpened() {
+        val str = "arduino opened..."
+        arduino!!.send(str.toByteArray())
+    }
+
+    override fun onUsbPermissionDenied() {
+        display("Permission denied. Attempting again in 3 sec...")
+        Handler().postDelayed({ arduino!!.reopen() }, 3000)
+    }
+
+    private fun display(message: String) {
+        runOnUiThread { displayTextView!!.append(message) }
+    }
+}
